ee6600c4f1b306919ffdc05c20568ca1f0e57cc7
Brine <brian@qualityshepherd.com>
2024-02-17 11:39:24 -0700

    words

--- src/views/about.js
+++ src/views/about.js
@@ -4,11 +4,10 @@ const about = {
       <h2>About</h2>
 
       <div class="center">
-        I'm a blabby nerd that lives in the desert.
+        <b>World of Brine</b> is a ttrpg podcast of words and worlds: meandering chats and actual play games.
         <div class="social">
-          <a href='&#109;a&#105;l&#116;o&#58;m%65%4&#48;b&#114;&#105;n%65&#46;%64e&#118;' title="email"><img src="assets/images/social/email.png" alt="email"></a>
-          <a href="https://casadeocio.itch.io/" title="games on itch"><img src="assets/images/social/itch-io.png" alt="my publications on itch"></a>
-          <a href="https://podcasts.apple.com/au/podcast/play-worlds-podcast/id1722152993" title="Play Worlds Podcast"><img src="assets/images/social/podcast.png" alt="Play Worlds Podcast"></a>
+          <a href='mai&#108;t&#111;&#58;w&#111;&#114;ldof%&#52;0b%&#55;2&#105;ne%2Ede&#118;' title="email"><img src="assets/images/social/email.png" alt="email"></a>
+          <a href="https://podcasts.apple.com/us/podcast/world-of-brine/id1547081865" title="World of Brine on Apple Podcasts"><img src="assets/images/social/podcast.png" alt="World of Brine"></a>
           <a href="https://www.youtube.com/channel/UCy0_3iGguFBabUOdQ2o_ZUQ" title="youtube"><img src="assets/images/social/youtube.png" alt="youtube"></a>
           <a href="/assets/rss/blog.xml"><img src="assets/images/social/rss.png" title="blog rss" alt="blog rss"></a>
         </div>
