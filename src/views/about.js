c014aec1557473bdcce93fec5344436178bd9ed6
Brine <brian@qualityshepherd.com>
2024-02-18 07:29:45 -0700

    social

--- src/views/about.js
+++ src/views/about.js
@@ -4,11 +4,13 @@ const about = {
       <h2>About</h2>
 
       <div class="center">
-        <b>World of Brine</b> is a ttrpg podcast of words and worlds: meandering chats and actual play games.
+        <b>World of Brine</b> is a ttrpg podcast of words and worlds: meandering chats and actual play games. Email your <i>kind</i> questions: <a href="mailto:w&#111;&#114;&#108;do&#102;&#64;&#98;rine&#46;&#100;&#101;&#118;">w&#111;&#114;&#108;do&#102;&#64;&#98;rine&#46;&#100;&#101;&#118;</a>
+
+        <p>Listen to the pod on your player of choice...
         <div class="social">
-          <a href='mai&#108;t&#111;&#58;w&#111;&#114;ldof%&#52;0b%&#55;2&#105;ne%2Ede&#118;' title="email"><img src="assets/images/social/email.png" alt="email"></a>
-          <a href="https://podcasts.apple.com/us/podcast/world-of-brine/id1547081865" title="World of Brine on Apple Podcasts"><img src="assets/images/social/podcast.png" alt="World of Brine"></a>
-          <a href="https://www.youtube.com/channel/UCy0_3iGguFBabUOdQ2o_ZUQ" title="youtube"><img src="assets/images/social/youtube.png" alt="youtube"></a>
+          <a href="https://podcasts.apple.com/us/podcast/world-of-brine/id1547081865" title="apple podcasts"><img src="assets/images/social/apple.png" alt="world of brine on apple podcasts"></a>
+          <a href="https://open.spotify.com/show/4T6OWe548efPrH6m6ZLrG4" title="spotify"><img src="assets/images/social/spotify.png" alt="world of brine on spotify"></a>
+          <a href="https://www.youtube.com/playlist?list=PLuYK1nk87vUzi0i1sNDdh__HWZ-EczMpj" title="youtube"><img src="assets/images/social/youtube.png" alt="world of brine on youtube"></a>
           <a href="/assets/rss/blog.xml"><img src="assets/images/social/rss.png" title="blog rss" alt="blog rss"></a>
         </div>
       </div>
