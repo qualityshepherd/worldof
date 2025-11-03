39450f67242574da540fc5163424135e864fd2bc
Brine <brian@qualityshepherd.com>
2025-11-03 16:16:19 -0700

    fix rss

--- src/config.js
+++ src/config.js
@@ -1,13 +1,13 @@
 const config = {
   title: 'World of Brine',
   description: 'An actual play ttrpg podcast...',
-  url: 'https://brine.dev',
+  url: 'https://worldof.brine.dev',
   pathToIndex: './index.json',
   pathToPostsFolder: './posts',
   pathToRssFolder: './assets/rss',
   maxPosts: 10,
   pod: {
-    image: 'https://brine.dev/assets/images/catface.png',
+    image: 'https://brine.dev/assets/images/brine_wide.jpg',
     author: 'brine',
     explicit: 'true',
     email: 'me@brine.dev',
